08f90fd5a580c8363852697efc99fbcf0fecb4e2
Mohamed Sallad Abdule <149385253+moha-shiine@users.noreply.github.com>
2024-11-17 16:34:21 +0300

    First commit

---
+++ lib/Singin.dart
@@ -0,0 +1,155 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:login/main.dart';
+
+import 'profile.dart';
+
+class Singin extends StatefulWidget {
+  const Singin({super.key});
+
+  @override
+  State<Singin> createState() => _SinginState();
+}
+
+class _SinginState extends State<Singin> {
+  static Future<User?> SinginEmailpasword(
+      {required String emailAddress,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    print(emailAddress + password);
+    try {
+      final credential = await auth.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+      user = credential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+    return user;
+  }
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Create an Account ",
+              style: TextStyle(
+                  fontSize: 33, fontWeight: FontWeight.w600, color: Colors.red),
+            ),
+            const SizedBox(
+              height: 06,
+            ),
+            const Text(
+              "Enter Your Contact",
+              style: TextStyle(
+                  //color: Colors.red.shade200,
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              controller: emailController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  prefixIcon: const Icon(IconlyBold.profile, color: Colors.red),
+                  hintText: "User Name"),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: emailController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  prefixIcon: const Icon(IconlyBold.message, color: Colors.red),
+                  hintText: "Emai@gmial.com"),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              controller: passwordController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.red, width: 1),
+                      borderRadius: BorderRadius.circular(16)),
+                  prefixIcon: const Icon(IconlyBold.lock, color: Colors.red),
+                  suffixIcon:
+                      const Icon(Icons.visibility_off, color: Colors.red),
+                  hintText: "password"),
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+            Container(
+              height: 60,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), color: Colors.red),
+              width: double.infinity,
+              child: InkWell(
+                onTap: () async {
+                  User? user = await SinginEmailpasword(
+                      emailAddress: emailController.text,
+                      password: passwordController.text,
+                      context: context);
+                  // User? user = await loginUsingEmailPassword(
+                  //     emailAddress: emailContorller.text,
+                  //     password: PasswordContorller.text,
+                  //     Context: context);
+                  if (user != null) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => const profile()));
+                  }
+                },
+                child: const Center(
+                    child: Text(
+                  "Sing In",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                )),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (_) => LoginScreen()));
+                },
+                child: Text(
+                  "Alredy User?" "Login",
+                  style: TextStyle(fontSize: 16),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
